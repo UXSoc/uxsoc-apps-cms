5e9f2a7ced682a9201381ee6967ae76507ca163d
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2021-11-05 23:30:36 +0800

    multiple airtable embed links

--- src/templates/apply-page/index.js
+++ src/templates/apply-page/index.js
@@ -1,25 +1,77 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Layout from '../../components/layout'
 import { graphql } from 'gatsby'
+import styled from "styled-components"
 
-export const ApplyPageTemplate = ({ link }) => (
-  <Fragment>
-    <iframe
-      className="airtable-embed"
-      title="airtable"
-      src={link}
-      frameborder="0"
-      onmousewheel=""
-      width="100%"
-      style={{
-        background: 'transparent',
-        border: '1px solid #ccc',
-        overflow: 'hidden',
-        height: '100vh',
-      }}
-    />
-  </Fragment>
-)
+const FormsContainer = styled.div`
+  .styled-tabs {
+    font-family: "Proxima Nova";
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .tab {
+    list-style: none;
+    padding: 10px;
+    color: gray;
+    text-transform: uppercase;
+
+    :hover{
+      cursor: pointer;
+    }
+  }
+  .active-tab {
+    color: black;
+    border-bottom: solid 3px gray;
+  }
+
+  & div {
+    background: url("https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=82a1493bq04qs3lwu2n8tbswlia2un9yjvc214lh6xg1p8g3&rid=200w.gif&ct=g") center center no-repeat;
+  }
+`
+
+export const ApplyPageTemplate = ({ links }) => {
+
+  const [tab, setTab] = useState(0)
+  const [currentLink, setCurrentLink] = useState(links[0])
+
+  useEffect(() => {
+    tab === 0 ? setCurrentLink(links[0]) : setCurrentLink(links[1])
+  }, [tab])
+
+  console.log(tab, currentLink)
+  
+  return (
+    <Fragment>
+      <FormsContainer>
+        <ul className="styled-tabs">
+          <li className={tab === 0 ? "tab active-tab" : "tab"} onClick={() => setTab(0)} active={tab === 0}>
+            Applying for AVP
+          </li>
+          <li className={tab === 1 ? "tab active-tab" : "tab"} onClick={() => setTab(1)} active={tab === 1}>
+            Applying for Pool
+          </li>
+        </ul>
+        <div>
+          <iframe
+            className="airtable-embed"
+            title="airtable"
+            src={currentLink}
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            style={{
+              background: 'transparent',
+              border: '1px solid #ccc',
+              overflow: 'hidden',
+              height: '100vh',
+            }}
+          />
+        </div>
+      </FormsContainer>
+    </Fragment>
+  )
+}
 
 const ApplyPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
@@ -29,8 +81,11 @@ const ApplyPage = ({ data }) => {
     primaryColor,
     footerColor,
     headerSection,
-    link,
+    links,
   } = frontmatter
+
+  console.log(links)
+
   return (
     <Layout
       slug={slug}
@@ -40,7 +95,7 @@ const ApplyPage = ({ data }) => {
       primary={primaryColor}
       footerColor={footerColor}
     >
-      <ApplyPageTemplate {...{ link }} />
+      <ApplyPageTemplate {...{ links }} />
     </Layout>
   )
 }
@@ -62,7 +117,7 @@ export const ApplyPageQuery = graphql`
           title
           buttonVisible
         }
-        link
+        links
       }
     }
   }
