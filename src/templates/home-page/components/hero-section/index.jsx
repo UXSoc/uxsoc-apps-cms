6ff028bbf41b62b78a21cf3f1fb1f573a4f8961b
jinyuanZapi <64624518+Jeoshyung@users.noreply.github.com>
2024-08-13 20:31:53 +0800

    added button functionality

--- src/templates/home-page/components/hero-section/index.jsx
+++ src/templates/home-page/components/hero-section/index.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import remark from 'remark';
 import recommended from 'remark-preset-lint-recommended';
 import remarkHtml from 'remark-html';
-import { Main, Button, HeroContainer } from './styles'
-import { Header, Container } from "../../../../components/styles"
+import { Link } from '../../../../components/Department/styles'
+import { Main, HeroContainer } from './styles'
+import { Header } from "../../../../components/styles"
 
 const HeroSection = ({ 
     heroSection, 
@@ -22,9 +23,9 @@ const HeroSection = ({
                         .use(remarkHtml)
                         .processSync(heroSection.description).toString()
                 }} />
-            <div className="text-center">
-                <Button primary={primaryColor} href="">Open Positions</Button>
-            </div>
+                <div style={{textAlign:'center'}}>
+                    <Link primary={primaryColor} href="#open-positions" style={{float:'none'}}>Open Positions</Link>
+                </div>
         </div>
     </HeroContainer>   
     )
